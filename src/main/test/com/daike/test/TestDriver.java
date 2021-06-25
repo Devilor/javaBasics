08a7129f684ed2448f981fbd7a3316bcc4f530e9
东街浊酒 <17663717998@163.com>
2021-06-25 16:34:10 +0800

    test

---
+++ src/main/test/com/daike/test/TestDriver.java
@@ -0,0 +1,24 @@
+package com.daike.test;
+
+import org.junit.Test;
+
+/**
+ * 简单测试类
+ *
+ * @author PigPig
+ * @date 2021/06/17 12:30
+ **/
+public class TestDriver {
+
+	@Test
+	public void testStringMatcher() {
+		String s = "Z_NAFP_C_BABJ_20210616002913_P_CLDAS_RT_ASI_0P0625_HOR-PRS-2021061600.nc";
+		System.out.println(s.matches(""));
+
+	}
+
+	@Test
+	public void testDemo() {
+
+	}
+}
